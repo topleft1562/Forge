69baffc8f557f06a883eb111c65c1dcd2c79d57d
Topleft <87454455+topleft1562@users.noreply.github.com>
2025-02-21 20:31:13 -0600

    Update confgi.ts

--- frontend/src/confgi.ts
+++ frontend/src/confgi.ts
@@ -1,6 +1,6 @@
 export const willMigrateAt = 500_000_000_000_000
 export const cluster = 'devnet'
 export const totalSupply = 1_000_000_000 // ( no decimals )
-export const INITIAL_PRICE = 10.0;
+export const INITIAL_PRICE = 0.01;
 export const PRICE_INCREMENT_STEP = 1_000_000;
-export const PRICE_INCREMENT = 0.0001;
+export const PRICE_INCREMENT = 0.001;
