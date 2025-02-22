49ca77bc2e9b8ac3368b77b225ba0fad79d541ed
Topleft <87454455+topleft1562@users.noreply.github.com>
2025-02-21 20:30:40 -0600

    Update config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,8 +1,8 @@
 export const ourFeeToKeep = 100_000_000
 export const willMigrateAt = 500_000_000_000_000
 export const cluster = 'devnet'
-export const INITIAL_PRICE = 10.0;
+export const INITIAL_PRICE = 0.01;
 export const PRICE_INCREMENT_STEP = 1_000_000;
-export const PRICE_INCREMENT = 0.0001;
+export const PRICE_INCREMENT = 0.01;
 export const totalSupply = 1_000_000_000_000_000;
 export const initialSOL = 30_000_000
