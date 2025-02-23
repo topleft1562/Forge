b02c9165037669845bc518df6c28d8bbd93d962a
Topleft <sworkman1562@gmail.com>
2025-02-23 08:18:19 -0600

    kk
    kk

--- Programs/Working/programs/consts/src/consts.rs
+++ Programs/Working/programs/consts/src/consts.rs
@@ -1,7 +1,6 @@
-// Constants
 pub const FEE_PERCENTAGE: f64 = 0.001; // 0.1% fee
 pub const PRICE_INCREMENT_STEP: u128 = 1_000_000; // Price increases every 1M tokens
-pub const INITIAL_PRICE: f64 = 0.1; // Starting price per token
-pub const PRICE_INCREMENT: f64 = 0.001; // Increase per step
+pub const INITIAL_PRICE: f64 = 0.0001; // Starting price per token
+pub const PRICE_INCREMENT: f64 = 0.000000000125; // Increase per step
 pub const SELL_REDUCTION: f64 = 0.99; // percent to reduce final sale price by
 pub const ADMIN: &str = "8Z7UgKvwfwtax7WjMgCGq61mNpLuJqgwY51yUgS1iAdF";
