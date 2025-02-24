2c9748d536e70a50ee33677b825a6020c823237c
Topleft <sworkman1562@gmail.com>
2025-02-23 19:44:02 -0600

    Update confgi.ts

--- frontend/src/confgi.ts
+++ frontend/src/confgi.ts
@@ -1,4 +1,4 @@
-export const willMigrateAt = 5_000_000_000
+export const willMigrateAt = 100_000_000_000
 export const cluster = 'devnet'
 export const totalSupply = 1_000_000_000_000_000 
 export const PRICE_INCREMENT_STEP = 1_000_000;
