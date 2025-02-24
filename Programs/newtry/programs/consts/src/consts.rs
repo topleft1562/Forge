bb7b16ccca9daa9f9d82ec55504d0fc295e5cb2a
Topleft <87454455+topleft1562@users.noreply.github.com>
2025-02-24 22:06:01 +0000

    kk

---
+++ Programs/newtry/programs/consts/src/consts.rs
@@ -0,0 +1,6 @@
+pub const FEE_PERCENTAGE: f64 = 0.001; // 0.1% fee
+pub const PRICE_INCREMENT_STEP: u128 = 1_000_000; // Price increases every 1M tokens
+pub const INITIAL_PRICE: f64 = 0.000028; // Starting price per token
+pub const SELL_REDUCTION: f64 = 0.99; // percent to reduce final sale price by
+pub const GROWTH_FACTOR: f64 = 1.0000000035;
+pub const ADMIN: &str = "8Z7UgKvwfwtax7WjMgCGq61mNpLuJqgwY51yUgS1iAdF";
