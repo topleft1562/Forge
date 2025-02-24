601f432ad2df375a5423243f047dd893120a8bad
Ryan Workman <workerbe@gmail.com>
2025-02-24 13:57:39 -0600

    should fix messages and a few errors i noticed

--- backend/src/routes/feedback.ts
+++ backend/src/routes/feedback.ts
@@ -8,35 +8,37 @@ const router = express.Router();
 // @route   GET /message/:
 // @desc    Get messages about this coin
 // @access  Public
-router.get('/coin/:coinId', (req, res) => {
+router.get("/coin/:coinId", (req, res) => {
     const coinId: string = req.params.coinId;
-    console.log(coinId)
-    Message.find({ coinId }).populate('coinId','sender').then(messages => res.status(200).send(messages))
-        .catch(err => res.status(400).json(err));
-})
+    console.log(coinId);
+    Message.find({ coinId })
+        .then((messages) => res.status(200).send(messages))
+        .catch((err) => res.status(400).json(err));
+});
 
 // @route   GET /message/:
 // @desc    Get messages about this user
 // @access  Public
-router.get('/user/:userId', (req, res) => {
+router.get("/user/:userId", (req, res) => {
     const sender: string = req.params.userId;
-    Message.find({ sender }).then(messages => res.status(200).send(messages))
-        .catch(err => res.status(400).json(err));
-})
+    Message.find({ sender })
+        .then((messages) => res.status(200).send(messages))
+        .catch((err) => res.status(400).json(err));
+});
 
 // @route   POST /message/
 // @desc    Save new Message
 // @access  Public
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
     const { body } = req;
-    console.log(body)
+    console.log(body);
     try {
         const newMsg = new Message(body);
-        const messages = await newMsg.save()
-        return res.status(200).send(messages)
+        const messages = await newMsg.save();
+        return res.status(200).send(messages);
     } catch (err) {
-        return res.status(400).json(err)
+        return res.status(400).json(err);
     }
-})
+});
 
 export default router;
