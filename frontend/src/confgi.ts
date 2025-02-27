3ca6d7a38867480c4581279af49bde32187cc11a
Topleft <sworkman1562@gmail.com>
2025-02-27 14:55:27 -0600

    kk
    kk

--- frontend/src/confgi.ts
+++ frontend/src/confgi.ts
@@ -1,10 +1,10 @@
 export const marketCapGoal = 50_000 
-export const cluster = 'devnet'
+export const cluster = 'mainnet'
 export const totalSupply = 1_000_000_000_000_000 
 export const PRICE_INCREMENT_STEP = 1_000_000;
 export const GROWTH_FACTOR = 1.0000000035;
-export const FEE_PERCENTAGE = 0.001;
+export const FEE_PERCENTAGE = 0.01;
 export const SELL_REDUCTION = 0.99
 export const INITIAL_PRICE = 0.000028;
 export const CREATEFEE = 100_000_000;
-export const ADMINKEY = "8Z7UgKvwfwtax7WjMgCGq61mNpLuJqgwY51yUgS1iAdF"
+export const ADMINKEY = "FhcQ7zoX7UUrLcNAfEY7joVsmu6jcFrcQWEJ4HY8DEDz"
