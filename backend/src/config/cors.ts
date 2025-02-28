903f87ea58a2dfa21f06dde39b0fba53c60a607e
Topleft <87454455+topleft1562@users.noreply.github.com>
2025-02-28 19:48:32 +0000

    try this for local and testnet

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -7,13 +7,14 @@ interface CorsConfig {
 const sitesThatNeedAccess = [
   "https://silver-train-76jpq9p4jgcp45g-3000.app.github.dev/",
   "https://forgemain.vercel.app/",
+  "https://forge-opal-psi.vercel.app/",
   // add others as needed
 ]
 // const frontendUrl = "*"  // allows everything
 
 const frontendUrl = process.env.MODE === 'local' 
 // set sites available to use backend locally running or testnet
-? ['http://localhost:3000', '*']
+? '*'
 // set sites available to use backend - production LIVE
 : sitesThatNeedAccess
 
