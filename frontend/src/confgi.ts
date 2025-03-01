544be81e077c23906a7afeca290d0c1f61ffbad4
Topleft <sworkman1562@gmail.com>
2025-02-28 19:29:39 -0600

    kk
    kk

--- frontend/src/confgi.ts
+++ frontend/src/confgi.ts
@@ -11,5 +11,5 @@ export const ADMINKEY = process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ?
  "4XcKzy92jkoasQ17TbRCD9xMGhsjZFBJPowWJyfXKuVd" : "8Z7UgKvwfwtax7WjMgCGq61mNpLuJqgwY51yUgS1iAdF"
 export const ourFeeToKeep = 5_000_000_000
 export const NEXT_PUBLIC_RPC_ENDPOINT = process.env.NEXT_PUBLIC_CHAIN === 'mainnet' ?
-    "https://devnet.helius-rpc.com/?api-key=ae50d21e-ae63-43d3-a23f-02cd8c93098c"
+    "https://mainnet.helius-rpc.com/?api-key=c00e3af0-ea96-491d-83a4-f00ec0a330e6"
     :'https://devnet.helius-rpc.com/?api-key=ae50d21e-ae63-43d3-a23f-02cd8c93098c'
